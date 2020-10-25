97d8db2a4d40322b42e12d3c3aa1ee5acfb6c99a
Liting-Mao <60677197+Liting-Mao@users.noreply.github.com>
2020-10-25 14:00:50 +0800

    Add carMove

---
+++ Assignment2/Assets/Scripts/CarMove.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarMove : MonoBehaviour
+{
+    public int speed = 5;
+    public float rotspeed = 30;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //car move
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward * vertical * speed * Time.deltaTime);
+        transform.Rotate(0, horizontal * rotspeed * Time.deltaTime, 0);
+    }
+}
