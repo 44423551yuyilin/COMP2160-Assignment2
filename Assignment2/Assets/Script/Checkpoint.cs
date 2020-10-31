ae21e715fda04b84142381715981325b3473a489
44423551yuyilin <yuyi.lin@students.mq.edu.au>
2020-10-31 11:06:16 +0800

    Checkpoint and PlayerHealth start

---
+++ Assignment2/Assets/Script/Checkpoint.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoint : MonoBehaviour
+{
+    public Renderer theRend;
+    public Material cpOn;
+    public Material cpOff;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void CheckpointOn()
+    {
+        theRend.material = cpOn;
+    }
+    public void CheckpointOff()
+    {
+        theRend.material = cpOff;
+    }
+}
