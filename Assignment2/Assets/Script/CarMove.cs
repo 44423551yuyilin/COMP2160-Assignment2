b672c1e8e14b383e3069701036412bb54ec06c54
Liting-Mao <60677197+Liting-Mao@users.noreply.github.com>
2020-11-04 14:26:33 +0800

    CarMove

---
+++ Assignment2/Assets/Script/CarMove.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarMove : MonoBehaviour
+{
+    public float speed = 15;
+    public float rotateSpeed = 90;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+
+        {
+
+            this.transform.Translate(Vector3.forward * Time.deltaTime * speed);
+
+        }
+
+        if (Input.GetKey(KeyCode.S))
+
+        {
+
+            this.transform.Translate(Vector3.back * Time.deltaTime * speed);
+
+        }
+
+        if (Input.GetKey(KeyCode.A))
+
+        {
+
+            this.transform.Rotate(Vector3.up * Time.deltaTime * -rotateSpeed);
+
+        }
+
+        if (Input.GetKey(KeyCode.D))
+
+        {
+
+            this.transform.Rotate(Vector3.up * Time.deltaTime * rotateSpeed);
+
+        }
+
+
+    }
+}
