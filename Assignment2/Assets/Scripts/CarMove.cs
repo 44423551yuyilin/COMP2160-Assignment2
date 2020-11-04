218119ccd7e8927208d0399bcbc11e32119ea760
Liting-Mao <60677197+Liting-Mao@users.noreply.github.com>
2020-11-04 13:49:27 +0800

    Revert "change"
    This reverts commit 0f84b157965210c076267f4d8d9fa6be8252ec98.

--- Assignment2/Assets/Scripts/CarMove.cs
+++ Assignment2/Assets/Scripts/CarMove.cs
@@ -4,65 +4,22 @@ using UnityEngine;
 
 public class CarMove : MonoBehaviour
 {
-    
-    public Rigidbody rigidbody;
-    public float driveSpeed;
-    public float rotateSpeed;
-
+    public int speed = 5;
+    public float rotspeed = 30;
 
     // Start is called before the first frame update
     void Start()
     {
-        rigidbody = GetComponent<Rigidbody>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-
-    }
-
-    void FixedUpdate()
-    {
         //car move
-        if (Input.GetKey(KeyCode.W))
-        {
-            rigidbody.AddForce(driveSpeed * Vector3.forward);
-
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            rigidbody.AddForce(driveSpeed * Vector3.back);
-
-        }
-
-        if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.A))
-        {
-            rigidbody.AddForce(driveSpeed * Vector3.forward);
-            this.transform.Rotate(Vector3.up * Time.deltaTime * -rotateSpeed);
-
-        }
-        if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.D))
-        {
-            rigidbody.AddForce(driveSpeed * Vector3.forward);
-            this.transform.Rotate(Vector3.up * Time.deltaTime * rotateSpeed);
-
-        }
-        if (Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.A))
-        {
-            rigidbody.AddForce(driveSpeed * Vector3.back);
-            this.transform.Rotate(Vector3.up * Time.deltaTime * rotateSpeed);
-
-        }
-        if (Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.D))
-        {
-            rigidbody.AddForce(driveSpeed * Vector3.back);
-            this.transform.Rotate(Vector3.up * Time.deltaTime * -rotateSpeed);
-
-        }
-
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward * vertical * speed * Time.deltaTime);
+        transform.Rotate(0, horizontal * rotspeed * Time.deltaTime, 0);
     }
-
-
-
 }
