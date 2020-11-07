44e0f4c7a44fd3a271020eb886498b9c626d9f6b
Liting-Mao <60677197+Liting-Mao@users.noreply.github.com>
2020-11-07 12:53:09 +0800

    Optimize carMove

--- Assignment2/Assets/Script/CarMove.cs
+++ Assignment2/Assets/Script/CarMove.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 
 public class CarMove : MonoBehaviour
 {
+    public Rigidbody rb;
     public float speed = 15;
     public float rotateSpeed = 90;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = gameObject.GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -19,7 +20,7 @@ public class CarMove : MonoBehaviour
         if (Input.GetKey(KeyCode.W))
 
         {
-
+            //rb.AddForce(Vector3.forward * speed);
             this.transform.Translate(Vector3.forward * Time.deltaTime * speed);
 
         }
@@ -27,27 +28,46 @@ public class CarMove : MonoBehaviour
         if (Input.GetKey(KeyCode.S))
 
         {
-
+            //rb.AddForce(Vector3.back * speed);
             this.transform.Translate(Vector3.back * Time.deltaTime * speed);
 
         }
 
-        if (Input.GetKey(KeyCode.A))
+        if (Input.GetKey(KeyCode.W)&&Input.GetKey(KeyCode.A))
 
         {
-
+            this.transform.Translate(Vector3.forward * Time.deltaTime * speed);
             this.transform.Rotate(Vector3.up * Time.deltaTime * -rotateSpeed);
 
         }
 
-        if (Input.GetKey(KeyCode.D))
+        if (Input.GetKey(KeyCode.W)&&Input.GetKey(KeyCode.D))
 
         {
-
+            this.transform.Translate(Vector3.forward * Time.deltaTime * speed);
             this.transform.Rotate(Vector3.up * Time.deltaTime * rotateSpeed);
 
         }
 
+        if(Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.A))
+        {
+            this.transform.Translate(Vector3.back * Time.deltaTime * speed);
+            this.transform.Rotate(Vector3.up * Time.deltaTime * rotateSpeed);
+        }
+
+
+        if (Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.D))
+
+        {
+            this.transform.Translate(Vector3.back * Time.deltaTime * speed);
+            this.transform.Rotate(Vector3.up * Time.deltaTime * -rotateSpeed);
+
+        }
+
+    }
+
+    void FixedUpdate()
+    {
 
     }
 }
